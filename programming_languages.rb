7d4cb07050282af272bfa2fa43ec89b88d8768fb
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 13:25:12 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
   # your code here
+  hash = {}
+  languages.each do |o_f, language|
+    language.each do |type, x|
+      style_array = []
+      style_array << o_f
+      hash[language] = {type => x, :style => style_array}
+    end
+  end  
+  hash
 end
