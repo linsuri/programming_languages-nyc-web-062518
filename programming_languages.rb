6702fcd252942166ec53152adb85760733d3e630
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 13:50:55 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -37,13 +37,15 @@ def reformat_languages(languages)
   hash = {}
   languages.each do |o_f, language|
     language.each do |type, x|
+      hash[language] = {}
+      
+      
       style_array = []
       style_array << o_f
       hash[language] = {type => x, style: style_array}
     end
   end  
   hash
-  binding.pry
 end
 
 reformat_languages(languages)
